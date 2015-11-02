ecf7e120686bd20056912d73c388c2b080575040
jakiboy98 <jakiboy98@gmail.com>
2015-11-02 09:38:58 -0700

    Changed Code to send texts

--- app/src/main/java/com/example/jwil9706/textroulette/TextRoulette.java
+++ app/src/main/java/com/example/jwil9706/textroulette/TextRoulette.java
@@ -1,6 +1,9 @@
 package com.example.jwil9706.textroulette;
 
+//Jacob W, Jacob A, Gabe A, Garret S, Alan H
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,131 +17,68 @@ import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 
-public class TextRoulette extends Activity
+public class MainActivity extends Activity
 {
-    private EditText messageText;
-    private TextView numberView;
-    private Button sendButton;
-    private Button randomNumberButton;
-    private Random randy;
-    private Random randy2;
-    private Random randy3;
-    private Random randy4;
-    private Random randy5;
-    private Random randy6;
-    private Random randy7;
-    private int finalNumber;
+
+    private Button shareIntent;
+    private Button send;
+    private EditText phoneNo;
+    private EditText messageBody;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_roulette);
 
-        messageText = (EditText) findViewById(R.id.messageText);
-        numberView = (EditText) findViewById(R.id.numberView);
-        sendButton = (Button) findViewById(R.id.sendButton);
-        randomNumberButton = (Button) findViewById(R.id.randomNumberButton);
-        randy = (Random) findViewById(R.id.randy);
-        randy = (Random) findViewById(R.id.randy2);
-        randy = (Random) findViewById(R.id.randy3);
-        randy = (Random) findViewById(R.id.randy4);
-        randy = (Random) findViewById(R.id.randy5);
-        randy = (Random) findViewById(R.id.randy6);
-        randy = (Random) findViewById(R.id.randy7);
-        finalNumber = (int) findViewById(R.id.finalNumber);
+        phoneNo = (EditText) findViewById(R.id.mobileNumber);
+        messageBody = (EditText) findViewById(R.id.smsBody);
 
-        setupListeners();
-
-    }
-
-    private void setupListeners()
-    {
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new OnClickListener()
+        {
             @Override
-            public void onClick(View currentView) {
-                try {
-                    String contact = numberView.getText().toString();
+            public void onClick(View v) {
+                String number = phoneNo.getText().toString();
+                String sms = messageBody.getText().toString();
 
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(number, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent!",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
             }
         });
 
-private void setupListeners()
-{
-    randomNumberButton.setOnClickListener(new View.OnClickListener)
-    {
-        randy =  (Math.random)*10);
-        randy2 =  (Math.random)*10);
-        randy3 =  (Math.random)*10);
-        randy4 =  (Math.random)*10);
-        randy5 =  (Math.random)*10);
-        randy6 =  (Math.random)*10);
-        randy7 =  (Math.random)*10);
-
-        finalNumber String = new String(+ randy + randy2 + randy3 + randy4 + randy5 + randy6 + randy7);
-
-    }
-}
-
-    public class SendSMSActivity extends Activity
-    {
-        Button sendButton;
-        EditText messageText;
-        TextView numberView;
-        Button randomNumberButton;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState)
+        shareIntent = (Button) findViewById(R.id.send);
+        shareIntent.setOnClickListener(new OnClickListener()
         {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
-
-            messageText = (EditText) findViewById(R.id.messageText);
-            numberView = (EditText) findViewById(R.id.numberView);
-            sendButton = (Button) findViewById(R.id.sendButton);
-            randomNumberButton = (Button) findViewById(R.id.randomNumberButton);
-
-            sendButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    String phoneNo = textPhoneNo.getText().toString();
-                    String sms = textSMS.getText().toString();
-
-                    try
-                    {
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage(phoneNo, null, sms, null, null);
-                        Toast.makeText(getApplicationContext(), "SMS Sent!", Toast.LENGTH_LONG).show();
-                    }
-                    catch (Exception e)
-                    {
-                        Toast.makeText(getApplicationContext(), "SMS failed, please try again later.", Toast.LENGTH_LONG).show();
-                        e.printStackTrace();
-                    }
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                    sendIntent.putExtra("sms_body", messageBody.getText().toString());
+                    sendIntent.setType("vnd.android-dir/mms-sms");
+                    startActivity(sendIntent);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
-            });
-        }
+            }
+        });
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_text_roulette, menu);
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
